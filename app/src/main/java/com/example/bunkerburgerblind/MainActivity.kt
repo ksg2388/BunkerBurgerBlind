062b1288c7d42ccd898011c682f4390aed3a0ef7
yunhee999 <yghqwer@gmail.com>
2022-11-30 22:05:56 +0900

    세트 주문하기->세트 종류 선택(UI 수정 필요)

--- app/src/main/java/com/example/bunkerburgerblind/MainActivity.kt
+++ app/src/main/java/com/example/bunkerburgerblind/MainActivity.kt
@@ -8,6 +8,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import com.example.bunkerburgerblind.databinding.ActivityMainBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -18,9 +21,29 @@ class MainActivity : AppCompatActivity() {
     private val database = Firebase.database
     private val myRef = database.getReference("bunkerburger")
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.orderSet.setOnClickListener {
+            //Dialog 만들기
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.set_choice, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("세트 주문하기")
+
+            val mAlertDialog = mBuilder.show()
+
+            val noButton = mDialogView.findViewById<Button>(R.id.cancel_button)
+            noButton.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+        }
 
         myRef.addValueEventListener(object: ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
