72b89e018b11c0d0ed0e112a0ebfac86e1426719
yeojiyoon <kkachi1025@gmail.com>
2022-12-15 14:50:57 +0900

    주문번호 기능 추가, database에 order 업로드

--- app/src/main/java/com/example/bunkerburgerblind/PaymentSucceedDig.kt
+++ app/src/main/java/com/example/bunkerburgerblind/PaymentSucceedDig.kt
@@ -27,6 +27,12 @@ class PaymentSucceedDig(context: Context) {
     fun PHCDig(SBList: ArrayList<shopping_basket_data>){
         database = Firebase.database.reference
         val myRef = database.child("bunkerburger").child("menu")
+        val orderRef = database.child("bunkerburger").child("order")
+
+
+        var key = ""
+        var id = 0
+        var iter = 1
 
         binding = PaymentSucceedDialogBinding.inflate(dialog.layoutInflater)
 
@@ -39,6 +45,16 @@ class PaymentSucceedDig(context: Context) {
 
         dialog.show()
 
+        orderRef.get().addOnSuccessListener {
+            for (ds in it.children){
+                key = ds.key as String
+            }
+            id = key.toInt() + 1
+
+            if(id<10) binding.orderId.text = "00" + id.toString()
+            else if(id<100) binding.orderId.text = "0" + id.toString()
+        }
+
         binding.ok.setOnClickListener {
             dialog.dismiss()
 
@@ -46,40 +62,68 @@ class PaymentSucceedDig(context: Context) {
                 if(item.type != "set") {
                     myRef.get().addOnSuccessListener {
                         val stock = it.child(item.type).child(item.name[0]).child("stock").value
-
-                        println(stock.toString() + "!!!!!!!!!!!!!!")
+                        val usage = it.child(item.type).child(item.name[0]).child("usage").value
 
                         val datapaths = myRef.child(item.type).child(item.name[0])
                         datapaths.child("stock").setValue(stock.toString().toInt() - item.cnt)
+                        datapaths.child("usage").setValue(usage.toString().toInt() + item.cnt)
                     }
+                    orderRef.get().addOnSuccessListener {
+                        for (ds in it.children){
+                            key = ds.key as String
+                        }
+                        id = key.toInt() + 1
+                        it.child("category")
+                    }
+                    val order = hashMapOf(
+                        "category" to item.type,
+                        "name" to item.name[0],
+                        "orderId" to id,
+                        "salePrice" to item.price,
+                        "cnt" to item.cnt
+                    )
+                    orderRef.child(id.toString()).child(iter.toString()).setValue(order)
                 }
                 else{
-                    println("1 " + item.name[0] + " 2 " + item.name[1] + " 3 " + item.name[2] + " 4 " + item.name[3])
                     myRef.get().addOnSuccessListener {
                         val stock = it.child("burger").child(item.name[1]).child("stock").value
+                        val usage = it.child("burger").child(item.name[1]).child("usage").value
 
-                        println(item.name[1] + " " + stock.toString())
                         val datapaths = myRef.child("burger").child(item.name[1])
                         datapaths.child("stock").setValue(stock.toString().toInt() - 1)
+                        datapaths.child("usage").setValue(usage.toString().toInt() + 1)
                     }
 
                     myRef.get().addOnSuccessListener {
                         val stock = it.child("side").child(item.name[2]).child("stock").value
+                        val usage = it.child("side").child(item.name[2]).child("usage").value
 
-                        println(item.name[2] + " " + stock.toString())
                         val datapaths = myRef.child("side").child(item.name[2])
                         datapaths.child("stock").setValue(stock.toString().toInt() - 1)
+                        datapaths.child("usage").setValue(usage.toString().toInt() + 1)
                     }
 
                     myRef.get().addOnSuccessListener {
                         val stock = it.child("beverage").child(item.name[3]).child("stock").value
+                        val usage = it.child("beverage").child(item.name[3]).child("usage").value
 
-                        println(item.name[3] + " " + stock.toString())
                         val datapaths = myRef.child("beverage").child(item.name[3])
                         datapaths.child("stock").setValue(stock.toString().toInt() - 1)
+                        datapaths.child("usage").setValue(usage.toString().toInt() + 1)
                     }
+
+                    val order = hashMapOf(
+                        "category" to item.type,
+                        "name" to item.name[0],
+                        "orderId" to id,
+                        "salePrice" to item.price,
+                        "cnt" to item.cnt
+                    )
+                    orderRef.child(id.toString()).child(iter.toString()).setValue(order)
                 }
+                iter++
             }
+
             SBList.clear()
 
             val intent = Intent(dialog.context, MainActivity::class.java)
