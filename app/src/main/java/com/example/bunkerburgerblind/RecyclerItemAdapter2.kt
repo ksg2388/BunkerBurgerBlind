88382b52e7071381fb1a71a21e5eb73a204a6c09
ksg2388 <ksg2388@naver.com>
2022-12-15 16:56:40 +0900

    세트메뉴 어느 부분이든 클릭 시 선택되도록 변경

--- app/src/main/java/com/example/bunkerburgerblind/RecyclerItemAdapter2.kt
+++ app/src/main/java/com/example/bunkerburgerblind/RecyclerItemAdapter2.kt
@@ -31,22 +31,18 @@ class RecyclerUserAdapter2(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.d("어댑터", items[0].name)
         val item = items[position]
-        val listener = View.OnClickListener { it ->
-            Toast.makeText(it.context, "Clicked -> name : ${item.name}, price : ${item.price}", Toast.LENGTH_SHORT).show()
-        }
-        holder.bind(item, listener)
+        holder.bind(item)
     }
 
     // 각 항목에 필요한 기능을 구현
     inner class ViewHolder(private val binding: ItemMenuListRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: MenuType, listener: View.OnClickListener) {
+        fun bind(item: MenuType) {
             binding.menuName.text = item.name
             binding.menuPrice.text = "+${item.price - 2500}"
             binding.menuExamination.text = item.examination
             binding.menuExamination.isSelected = true
             Glide.with(itemView).load(item.img).into(binding.menuImg)
-            binding.root.setOnClickListener(listener)
 
             binding.menuCheckbox.isChecked = adapterPosition == selectedPosition
             binding.menuCheckbox.setOnClickListener {
@@ -55,9 +51,12 @@ class RecyclerUserAdapter2(
                 link.getSelectedIndex(adapterPosition)
                 notifyDataSetChanged()
             }
-
-            Log.d("포지션", adapterPosition.toString())
-            Log.d("포지션", selectedPosition.toString())
+            binding.root.setOnClickListener {
+                Toast.makeText(it.context, "${item.name}이(가) 선택되었습니다.", Toast.LENGTH_SHORT).show()
+                selectedPosition = adapterPosition
+                link.getSelectedIndex(adapterPosition)
+                notifyDataSetChanged()
+            }
         }
     }
 
--- app/src/main/java/com/example/bunkerburgerblind/RecyclerItemAdapter3.kt
+++ app/src/main/java/com/example/bunkerburgerblind/RecyclerItemAdapter3.kt
@@ -24,29 +24,23 @@ class RecyclerUserAdapter3(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemMenuListRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        Log.d("어댑터", "연결!!")
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Log.d("어댑터", items[0].name)
         val item = items[position]
-        val listener = View.OnClickListener { it ->
-            Toast.makeText(it.context, "Clicked -> name : ${item.name}, price : ${item.price}", Toast.LENGTH_SHORT).show()
-        }
-        holder.bind(item, listener)
+        holder.bind(item)
     }
 
     // 각 항목에 필요한 기능을 구현
     inner class ViewHolder(private val binding: ItemMenuListRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: MenuType, listener: View.OnClickListener) {
+        fun bind(item: MenuType) {
             binding.menuName.text = item.name
             binding.menuPrice.text = "+${item.price - 2000}"
             binding.menuExamination.text = item.examination
             binding.menuExamination.isSelected = true
             Glide.with(itemView).load(item.img).into(binding.menuImg)
-            binding.root.setOnClickListener(listener)
 
             binding.menuCheckbox.isChecked = adapterPosition == selectedPosition
             binding.menuCheckbox.setOnClickListener {
@@ -55,9 +49,12 @@ class RecyclerUserAdapter3(
                 link.getSelectedIndex(adapterPosition)
                 notifyDataSetChanged()
             }
-
-            Log.d("포지션", adapterPosition.toString())
-            Log.d("포지션", selectedPosition.toString())
+            binding.root.setOnClickListener {
+                Toast.makeText(it.context, "${item.name}이(가) 선택되었습니다.", Toast.LENGTH_SHORT).show()
+                selectedPosition = adapterPosition
+                link.getSelectedIndex(adapterPosition)
+                notifyDataSetChanged()
+            }
         }
     }
 
